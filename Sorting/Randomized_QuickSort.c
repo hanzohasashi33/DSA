40d32e2dfea4badff2b312711e79398e6e5ed3f9
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2020-02-01 16:56:38 +0530

    Create Randomized_QuickSort.c

---
+++ Sorting/Randomized_QuickSort.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//#define swap(x,y) int t=x;x=y;y=t;
+
+void swap(int *a,int *b)
+{
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+
+
+
+int partition(int arr[],int i,int j,int pivot)
+{
+    int l=i;
+    int r=j;
+    while(l<=r)
+    {
+        while(l<=r && arr[l]<=pivot) 
+            l++;
+        while(l<=r && arr[r]>pivot) 
+            r--;
+        if(l<=r) 
+        {
+            swap(&arr[l],&arr[r]);
+            l++; r--;
+        }
+    }
+    int k = l-1;
+    swap(&arr[i-1],&arr[k]);
+    arr[k] = pivot;
+    return k;
+}
+void Rquicksort(int arr[],int i,int j)
+{
+    if(i<j)
+    {
+        int pivot_index=rand()%(j-i+1)+i;
+        swap(&arr[i],&arr[pivot_index]);
+        int k=partition(arr,i+1,j,arr[i]);
+        Rquicksort(arr,i,k-1);
+        Rquicksort(arr,k+1,j);
+    }
+}
+
+
+
+
+int main(void)
+{
+  int n;
+  scanf("%d",&n);
+  int arr[n];
+  for(int i = 0;i < n;i++)
+  {
+    scanf("%d",&arr[i]);
+  }
+    Rquicksort(arr,0,n - 1);
+    for(int i=0;i < n;i++)
+        printf("%d ",arr[i]);
+    return 0;
+}
