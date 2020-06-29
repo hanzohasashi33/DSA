f421a1e466ef2e313e2b2c90b5e1af5149709f8f
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-06-29 21:04:10 +0530

    Update BFS.py

--- Graphs/BFS.py
+++ Graphs/BFS.py
@@ -10,20 +10,28 @@ class Graph :
 
     
     def BFS(self,s) :
+        distance = [float("inf")] * len(self.graph)
         visited = [False] * len(self.graph)
         queue = []
 
         queue.append(s)
         visited[s] = True
+        distance[s] = 0
 
         while queue :
             s = queue.pop(0)
-            print(s,end = " ")
+            print(s,distance[s])
 
             for i in self.graph[s] :
                 if visited[i] == False :
                     queue.append(i)
                     visited[i] = True 
+                    distance[i] = distance[s] + 1
+
+
+    def printgraph(self) :
+        for i in self.graph :
+            print(i,self.graph[i])
 
 
 if __name__ == "__main__" :
@@ -34,6 +42,6 @@ if __name__ == "__main__" :
     g.addedge(2, 0) 
     g.addedge(2, 3) 
     g.addedge(3, 3) 
-  
+    g.printgraph()
     print ("Following is Breadth First Traversal" " (starting from vertex {})".format("2")) 
     g.BFS(2) 
